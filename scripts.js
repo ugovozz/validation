6f69db5a9a1ecf222fc187f6e5f173962647eb25
Hugo Alvarez <ugovozz@hotmail.com>
2019-01-08 21:01:32 -0300

    Validate Input largo

--- scripts.js
+++ scripts.js
@@ -1,2 +1,16 @@
 const passwordInput = document.getElementById('password-input');
 const submitButton = document.getElementById('submit-button');
+
+passwordInput.addEventListener('input', ($event) => {
+
+    if ($event.target.value.length > 5 && $event.target.value.length < 13) {
+        submitButton.removeAttribute('disabled');
+    } else if ($event.target.value.length < 6 || $event.target.value.length > 12) {
+
+        if (submitButton.disable != true) {
+            console.log("EVENTO");
+            submitButton.setAttribute('disabled', true);
+        }
+
+    }
+});

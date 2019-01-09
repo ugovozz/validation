2b4d7fc298fc09a2e612df25b3085120076b6f4d
Hugo Alvarez <ugovozz@hotmail.com>
2019-01-08 21:14:37 -0300

    validate password Blur event

--- scripts.js
+++ scripts.js
@@ -1,16 +1,12 @@
 const passwordInput = document.getElementById('password-input');
-const submitButton = document.getElementById('submit-button');
+const confirmPassword = document.getElementById('confirm-password');
+const errorMsg = document.getElementById('error-message');
 
-passwordInput.addEventListener('input', ($event) => {
-
-    if ($event.target.value.length > 5 && $event.target.value.length < 13) {
-        submitButton.removeAttribute('disabled');
-    } else if ($event.target.value.length < 6 || $event.target.value.length > 12) {
-
-        if (submitButton.disable != true) {
-            console.log("EVENTO");
-            submitButton.setAttribute('disabled', true);
-        }
-
+confirmPassword.addEventListener('blur', ($event) => {
+    if (passwordInput.value !== confirmPassword.value) {
+        errorMsg.style.display = "block";
+    } else {
+        errorMsg.style.display = "none";
+        confirmPassword.style.border = "thin solid green";
     }
 });
